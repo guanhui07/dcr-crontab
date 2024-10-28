28f8f7aca7e1d6cb33b167e88929a65e5e658aa5
yangminghui <guanhui07@gmail.com>
2024-10-28 16:59:35 +0800

    add Log

---
+++ src/Utils/Log.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace DcrSwooleCrontab\Utils;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+class Log
+{
+    public function info($str): void
+    {
+        $log = $this->getLogger(Logger::INFO);
+        $log->warning((string)$str);
+    }
+
+    public function debug($str): void
+    {
+        $log = $this->getLogger(Logger::INFO);
+        $log->warning((string)$str);
+    }
+
+    public function warning($str): void
+    {
+        $log = $this->getLogger(Logger::WARNING);
+        $log->warning((string)$str);
+    }
+
+    public function error($str): void
+    {
+        $log = $this->getLogger(Logger::ERROR);
+        // add records to the log
+        $log->error((string)$str);
+    }
+
+    public function write($str, $config = ''): void
+    {
+        $this->error($str);
+    }
+
+    /**
+     * @param int $level
+     * @return Logger
+     */
+    protected function getLogger($level = Logger::WARNING): Logger
+    {
+        $log = new Logger('name');
+        $log->pushHandler(new StreamHandler(base_path() . 'runtime/log.log', $level));
+        return $log;
+    }
+
+    /**
+     * @param string $method
+     * @param array $arguments
+     *
+     * @return static
+     */
+    public static function __callStatic(string $method, array $arguments)
+    {
+        return (new static)->{$method}(...$arguments);
+    }
+
+}
